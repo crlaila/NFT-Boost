7ac0ab9d25e6b2c811f2884c3dc56994279aedf2
Laila Silva <laila.cristine@outlook.com>
2024-09-06 22:57:55 -0300

    feat: componente title

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import { ContainerGrid } from "@/components/container";
+import { TitleSection } from "@/components/title-section";
 
 export default function Home() {
   return (
-    <ContainerGrid>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-    </ContainerGrid>
+    <>
+      <section>
+        <ContainerGrid>
+          <TitleSection
+            title={'Coleções notáveis'}
+            subtitle={'Destaque em Coleções'}
+          />
+        </ContainerGrid>
+      </section>
+    </>
   );
 }
